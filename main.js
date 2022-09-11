3b91744249e8f40c3d2b159b75052b3bc1bda0b7
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 20:53:14 +0530

    indexOf

---
+++ main.js
@@ -0,0 +1,7 @@
+let text = "Hello world, welcome to the universe.";
+console.log("original array :", text);
+console.log("length of the array is "+text.length);
+console.log("method : array.indexOf(world)");
+let result = text.indexOf("world");
+console.log("change in array : ", result);
+console.log("return: length of array after indexOf() is "+text.length);
